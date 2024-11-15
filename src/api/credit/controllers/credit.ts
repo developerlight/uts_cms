eaf3935c3c2362c5d124459b0e859ddbbb6b434f
developerlight <samsulmuhyidin@gmail.com>
2024-11-15 21:55:18 +0700

    first commit

---
+++ src/api/credit/controllers/credit.ts
@@ -0,0 +1,7 @@
+/**
+ * credit controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::credit.credit');
---
+++ src/api/motorcycle/controllers/motorcycle.ts
@@ -0,0 +1,7 @@
+/**
+ * motorcycle controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::motorcycle.motorcycle');
